f0a6ce715ffb1645955e8e2b7e9529e110109207
wambugubilha <bilhahhwamb@gmail.com>
2025-07-15 11:35:30 +0300

    commit basic_operations.py file

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Assign values to variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print the results in a human-readable format
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
