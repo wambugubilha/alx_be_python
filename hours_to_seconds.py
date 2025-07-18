b1ea1acf50a0188c6f89cd79f02a6f3d619aa0ef
wambugubilha <168825087+wambugubilha@users.noreply.github.com>
2025-07-18 16:10:27 +0300

    Delete hours_to_seconds.py

--- hours_to_seconds.py
+++
@@ -1,8 +0,0 @@
-# Define the number of hours
-hours = 2
-
-# Convert hours to seconds (3600 seconds in one hour)
-seconds = hours * 3600
-
-# Print the result
-print(f"{hours} hour(s) is {seconds} seconds")
