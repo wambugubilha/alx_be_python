cc9aed0d6a09d1d6ac170a097316dbe17d7ab636
wambugubilha <bilhahhwamb@gmail.com>
2025-07-27 16:52:26 +0300

    commit the arithmetic operations modified file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,8 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+# arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
